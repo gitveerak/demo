b6a2849daeeda5723369a8c0e5e951ad7f53355f
gitveerak <markvicky467@gmail.com>
2025-02-05 16:58:10 +0530

    second commit

--- program/app.py
+++ program/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+import os
 
 app=Flask(__name__)
 
@@ -12,4 +13,5 @@ def login():
     return render_template('login.html')
 
 if __name__== '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get the port from the environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port)
